78e7ad10fa3c2a0a310e6a745ff240eb3ee3549a
Ryan Mugo <gathariryan@gmail.com>
2024-01-24 13:47:08 +0100

    edit of homepage, creation of about page complete

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,10 +1,10 @@
 const About = () => {
   return (
     <div className="h-screen flex flex-col justify-start bg-gray-100">
-      <h1 className="text-3xl sm:text-5xl font-semibold underline text-orange-300 text-center mt-20">
+      <h1 className="text-3xl sm:text-5xl font-semibold underline text-orange-400 text-center mt-20">
         About
       </h1>
-      <p className="mt-14 mb-12 text-center text-orange-600 ">
+      <p className="mt-14 mb-12 text-center font-semibold text-orange-500">
         Since its inception in 2009, Alsasha Investments Ltd has rapidly grown
         into a prominent player in the real estate sector. Specializing in
         common area facility management for residential and commercial units, we
@@ -12,13 +12,46 @@ const About = () => {
         grounds, power, plumbing, lighting, air conditioning, cleaning, and
         security.
       </p>
-      <div className="ml-20 sm:mx-auto grid grid-cols-2 gap-4 place-items-center p-6 bg-orange-500 border rounded-lg">
-        <img src="main2.jpg" alt="Image 1" className="w-[500px] h-[240px]" />
-        <img src="main3.jpg" alt="Image 2" className="w-[500px] h-[240px]" />
+      <div className="grid grid-cols-2 gap-2  place-items-center">
+        <img
+          src="main3.jpg"
+          alt="Image 1"
+          className="w-full md:w-[400px] h-auto"
+        />
+
+        <img
+          src="mow.jpg"
+          alt="Image 2"
+          className="w-full md:w-[400px] h-auto"
+        />
       </div>
-      <p className="mb-4 text-center text-slate-700"></p>
-      <p className="mb-4 text-center text-slate-700"></p>
-      <p className="mt-5 mb-2 text-black text-center font-bold"></p>
+      <p className="mt-4 text-center text-orange-500 font-semibold text-lg">
+        Elevating Excellence: Where Every Detail Is Crafted to Perfection!
+      </p>
+      <br />
+      <br />
+      <p className="mt-2 text-center font-semibold text-orange-500">
+        At Alsasha Investments Ltd, we prioritize safety, security,
+        cost-effectiveness, and environmental responsibility in all our
+        operations. Our dedicated team ensures the seamless functioning of real
+        estate, focusing on the long-term preservation of assets.
+      </p>
+      <p className="mt-2  text-orange-500 text-center font-semibold">
+        Beyond basic facility management, we extend our services to landscaping
+        and janitorial tasks, creating a clean, sanitized, and pleasant
+        environment for residents. Our commitment to excellence makes Alsasha
+        Investments Ltd the preferred choice for those seeking top-notch
+        expertise in common area facility management.
+      </p>
+      <br />
+
+      <p className="mt-2  text-orange-500 text-center font-bold">
+        Embark on a journey with us to transform spaces into vibrant havens that
+        withstand the sands of time! 🌟🏡 <br /> <br />
+        Let's create lasting impressions through meticulous care and innovation.{" "}
+        <br /> Together, we build environments that thrive! <br /> <br /> 🌿💼
+        #TransformingSpaces &nbsp; &nbsp; #TimelessExcellence
+      </p>
     </div>
   );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,19 +3,19 @@ const Home = () => {
     <div className="h-screen w-full">
       <div
         className="bg-cover bg-center h-screen flex items-center justify-center"
-        style={{ backgroundImage: 'url("/cleanliness.jpg")' }}
+        style={{ backgroundImage: 'url("/tip.jpg")' }}
       >
-        <div className="flex items-center justify-center text-center gap-6">
-          <h1 className="text-4xl  font-bold">
+        <div className="flex flex-col items-center justify-center text-center gap-6">
+          <h1 className="text-2xl md:text-4xl lg:text-5xl text-orange-400 font-bold">
             🌟 Transforming Chaos into Clean Spaces, One Step at a time! 🧹✨
           </h1>
-          <p className="p-4 bg-orange-300 text-xl text-center border rounded">
-            Vision: Be a leader in provision of efficient and affordable
+          <p className="p-4 bg-orange-300 text-md md:text-xl text-center border rounded">
+            Vision: Be a leader in the provision of efficient and affordable
             services in common area facility management
             <br />
             <br />
-            Mission: To provide effective and quality services that enable
-            clean, robust and friendly environment
+            Mission: To provide effective and quality services that enable a
+            clean, robust, and friendly environment
           </p>
         </div>
       </div>
