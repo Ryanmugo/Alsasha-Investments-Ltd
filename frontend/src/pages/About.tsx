e34674da8ecdaa1cd32ce920689685ec6cbe73ae
Ryan Mugo <gathariryan@gmail.com>
2024-01-24 14:55:32 +0100

    a little tuneups complete

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div className="h-screen flex flex-col justify-start bg-gray-100">
-      <h1 className="text-3xl sm:text-5xl font-semibold underline text-orange-400 text-center mt-20">
+      <h1 className="text-3xl sm:text-5xl font-normal underline text-orange-400 text-center mt-20">
         About
       </h1>
       <p className="mt-14 mb-12 text-center font-semibold text-orange-500">
@@ -45,6 +45,75 @@ const About = () => {
       </p>
       <br />
 
+      <div className="flex flex-col items-center justify-center text-center gap-6">
+        <h1 className="text-xl md:text-4xl lg:text-5xl text-orange-400 font-bold">
+          🌐 Our Core Values ✨
+        </h1>
+        <div className="p-4 bg-orange-300 text-md md:text-xl  border rounded">
+          <ul>
+            <li className="font-bold">
+              1. Building Long-Term Relationships: &nbsp;&nbsp;
+              <p className="font-normal">
+                We prioritize cultivating enduring connections with our clients,
+                partners, and stakeholders. By fostering trust and mutual
+                respect, we lay the foundation for sustained collaboration and
+                success.
+              </p>
+            </li>
+            <br />
+            <li className="font-bold">
+              2. Quality Service and Professionalism: &nbsp; &nbsp;
+              <p className="font-normal">
+                Dedication to delivering top-notch service with a high level of
+                professionalism is at the heart of our operations. We strive for
+                excellence in every aspect, ensuring our clients receive
+                services that exceed expectations.
+              </p>
+            </li>
+            <br />
+            <li className="font-bold">
+              3. Customer-Focused and Solution-Driven: &nbsp; &nbsp;
+              <p className="font-normal">
+                Our focus revolves around understanding and addressing the
+                unique needs of our customers. We are committed to providing
+                tailored solutions, ensuring customer satisfaction is at the
+                forefront of every decision and action we take.
+              </p>
+            </li>
+            <br />
+            <li className="font-bold">
+              4. Integrity: &nbsp; &nbsp;
+              <p className="font-normal">
+                Upholding the highest standards of honesty and integrity is
+                non-negotiable for us. We conduct our business with transparency
+                and ethical practices, fostering trust among our clients and
+                stakeholders.
+              </p>
+            </li>
+            <br />
+            <li className="font-bold">
+              5. Discipline and Teamwork: &nbsp; &nbsp;
+              <p className="font-normal">
+                We believe in the power of discipline to drive consistency and
+                excellence. Combined with a strong spirit of teamwork, we
+                leverage diverse skills and perspectives to achieve collective
+                success and overcome challenges.
+              </p>
+            </li>
+            <br />
+            <li className="font-bold">
+              6. Accountability and Transparency: &nbsp; &nbsp;
+              <p className="font-normal">
+                Taking ownership of our actions and being transparent in our
+                processes are fundamental values. We believe in being
+                accountable for our commitments, and we communicate openly to
+                build a culture of trust and reliability.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
       <p className="mt-2  text-orange-500 text-center font-bold">
         Embark on a journey with us to transform spaces into vibrant havens that
         withstand the sands of time! 🌟🏡 <br /> <br />
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,10 +6,16 @@ const Home = () => {
         style={{ backgroundImage: 'url("/tip.jpg")' }}
       >
         <div className="flex flex-col items-center justify-center text-center gap-6">
-          <h1 className="text-2xl md:text-4xl lg:text-5xl text-orange-400 font-bold">
+          <h1
+            className="text-2xl md:text-4xl lg:text-5xl text-orange-400 font-bold"
+            style={{
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+            }}
+          >
             🌟 Transforming Chaos into Clean Spaces, One Step at a time! 🧹✨
           </h1>
-          <p className="p-4 bg-orange-300 text-md md:text-xl text-center border rounded">
+
+          <p className="p-4 bg-orange-300 text-md md:text-2xl text-center border rounded">
             Vision: Be a leader in the provision of efficient and affordable
             services in common area facility management
             <br />
